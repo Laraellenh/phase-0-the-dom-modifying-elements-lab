d506c7f4dbe3f6a6c6d69d5fbf4835f37d9e12d6
Lara <laraellenh@gmail.com>
2022-01-16 18:47:48 -0500

    commit tree 59b8b6bd5af2b293b532167d33d732c9e2c8dfc8

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // Write your code here!
-const main = document.querySelector('main')
 
-main.remove()
+main.remove();
 
 const newHeader = document.createElement('h1')
-
-newHeader.setAttribute("id", "victory")
-
-newHeader.innerHTML = "Lara is the champion"
-
-document.body.append(newHeader);
-
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "LARA is the champion"
